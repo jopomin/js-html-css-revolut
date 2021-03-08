b6d5ee9da99f78e4e171ef55e0b78d5fa9b4ae54
Giovanni <giovannipor23@gmail.com>
2021-03-08 18:39:29 +0100

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+    $('.dropdown').mouseenter(function() {
+        $(this).find('.dd_menu').addClass('visible');
+        $(this).find('> a').addClass('active');
+    })
+
+    $('.dropdown').mouseleave(function() {
+        $(this).find('.dd_menu').removeClass('visible');
+        $(this).find('> a').removeClass('active');
+    })
 
 })
